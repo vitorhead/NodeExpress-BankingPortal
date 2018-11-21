2ab028476ff33b29201ab198ef3018b4c8ebd273
Vitor de Santiago <vitor.de.santiago@avanade.com>
2018-11-21 11:49:23 -0200

    File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const { promisify } = require('util')
 
 const app = express()
+
+const readFileAsPromise = promisify(fs.readFile)
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => res.render('index', { title: 'Index Page' }))
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8')
+const accounts = JSON.parse(accountData)
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8')
+const users = JSON.parse(userData)
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }))
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }))
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }))
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }))
+app.get('/profile', (req, res) => res.render('profile', { user: users[0] }))
 
 app.listen(3000, () => console.log('Running on 3000'))
