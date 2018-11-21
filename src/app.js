728f88c66ed1025e73824b8a8250811298615bdd
Vitor Stipanich <vitorstipanich@hotmail.com>
2018-11-21 13:55:50 -0200

    Handling Form Data

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
 
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8')
 const accounts = JSON.parse(accountData)
@@ -22,5 +23,27 @@ app.get('/savings', (req, res) => res.render('account', { account: accounts.savi
 app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }))
 app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }))
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }))
+app.get('/transfer', (req, res) => res.render('transfer'))
+app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }))
+
+app.post('/transfer', (req, res) => {
+    const { from, to, amount } = req.body
+    accounts[from].balance -= amount
+    accounts[to].balance = parseInt(accounts[to].balance) + parseInt(amount)
+
+    const accountsJSON = JSON.stringify(accounts)
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+    res.render('transfer', { message: 'Transfer Completed' })
+})
+
+app.post('/payment', (req, res) => {
+    const { amount } = req.body
+    accounts.credit.balance -= amount
+    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(amount)
+
+    const accountsJSON = JSON.stringify(accounts)
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+    res.render('payment', { message: 'Payment completed', account: accounts.credit })
+})
 
 app.listen(3000, () => console.log('Running on 3000'))
